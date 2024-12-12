a7374042142a9cdf3b2c1dc99a41bc2ebe81f726
andpet12 <andrpetr18912@gmail.com>
2024-12-12 15:14:01 +0200

    lesson1

---
+++ lesson1/HW-TS3/main.ts
@@ -0,0 +1,5 @@
+    let firstName: string = 'Andrii';
+    let middleName: string = 'Petrovych';
+    let lastName: string = 'Stadnyk';
+    const person: string = `${firstName}  ${middleName} ${lastName}`;
+    console.log(person);
